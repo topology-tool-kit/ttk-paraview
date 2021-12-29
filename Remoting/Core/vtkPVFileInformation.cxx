6615c28912b21fbc6762e5ca3d4da69c708051f7
Julien Tierny <julien.tierny@gmail.com>
2021-12-29 14:39:45 +0100

    Patched sources

--- Remoting/Core/vtkPVFileInformation.cxx
+++ Remoting/Core/vtkPVFileInformation.cxx
@@ -1201,11 +1201,7 @@ std::string vtkPVFileInformation::GetParaViewSharedResourcesDirectory()
 
   // Where docs might be in relation to the executable
   std::vector<std::string> prefixes = {
-#if defined(_WIN32) || defined(__APPLE__)
-    ".."
-#else
     "share/paraview-" PARAVIEW_VERSION
-#endif
   };
 
   // Search for the docs directory
@@ -1215,6 +1211,11 @@ std::string vtkPVFileInformation::GetParaViewSharedResourcesDirectory()
   {
     resource_dir = vtksys::SystemTools::CollapseFullPath(resource_dir);
   }
+
+  vtkProcessModule* pm = vtkProcessModule::GetProcessModule();
+
+  if((pm)&&(prefixes.size()))
+    resource_dir = pm->GetSelfDir() + "/../" + prefixes[0];
 
   return resource_dir;
 }
